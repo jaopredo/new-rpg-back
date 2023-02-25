9a518ceebcf6a43e3d591aaf03e81147032d9d21
João Pedro <joao.profissional.45@gmail.com>
2023-02-24 23:57:00 -0300

    Correção erro grave no login

--- src/routes/playerRoutes.js
+++ src/routes/playerRoutes.js
@@ -33,7 +33,10 @@ router.post('/login', async (req, res) => {
             return res.json(sendStatus(0, "Esse email não está no banco"))
         }
         const player = await Player.findOne({ email: req.body.email }).select("+password")
-        return res.json({token: generateToken({ id: player.id })})
+        if (player.password === req.body.password) {
+            return res.json({token: generateToken({ id: player.id })})
+        }
+        return res.json(sendStatus(0, "A senha está incorreta!"))
     } catch (e) {
         return res.json(sendStatus(0))
     }
